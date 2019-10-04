10adc51414a93e7cd0b08759c8d14c520f798f84
JPCASATI-VAIO <jpcasati@usp.br>
2019-10-03 21:21:55 -0300

    Ação do botão TREM programada.

--- app/src/main/java/br/edu/mouralacerda/gamgithub/MainActivity.java
+++ app/src/main/java/br/edu/mouralacerda/gamgithub/MainActivity.java
@@ -2,12 +2,28 @@ package br.edu.mouralacerda.gamgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    ImageButton trem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trem = findViewById(R.id.btnTrem);
+
+        trem.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Piuíííí...", Toast.LENGTH_SHORT).show();
     }
 }
